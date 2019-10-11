d11e6d90b65e28dc1d28411b5eaebfebd4a8f72e
Arunraj Adlee <arunraj.adlee@hotmail.com>
2019-10-11 17:35:21 -0400

    Initial front-end skeleton setup

---
+++ Front-End/my-app/src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Switch, Route } from 'react-router';
+import './index.css';
+import { createBrowserHistory } from 'history';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+const history = createBrowserHistory();
+
+
+ReactDOM.render(
+  <Router history={history}>
+    <Switch>
+      <Route exact path="/">
+        <App />
+      </Route>
+    </Switch>
+  </Router>, document.getElementById('root'),
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
