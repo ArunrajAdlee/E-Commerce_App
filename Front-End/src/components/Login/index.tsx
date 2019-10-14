63986c42862699c1bd54d4df30a1f4081aacf011
Arunraj Adlee <arunraj.adlee@hotmail.com>
2019-10-13 20:53:58 -0400

    Add SASS and bootstrap ui library

---
+++ Front-End/src/components/Login/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+interface IStates {
+  user: string,
+  pass: string,
+}
+
+
+class Login extends React.Component<{}, IStates> {
+  public readonly state: Readonly<IStates> = {
+    user: '',
+    pass: '',
+  }
+
+  public componentDidMount() {
+  }
+
+  private handleSubmit = () => {
+    //
+  }
+
+  public render() {
+    const { user, pass } = this.state;
+    // Testing bootstrap...
+    return (
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    );
+  }
+}
+
+export default Login;
