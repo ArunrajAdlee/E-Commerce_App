5fa6a4dd8f6731163ae12a1a1f6137b006d6420e
Radhep <Radhep.Saba@gmail.com>
2019-10-13 21:14:35 -0400

    changes

--- Back-End/README.md
+++ Back-End/README.md
@@ -1,7 +1,8 @@
-# Awesome Project Build with TypeORM
+# Backend of Team Awesome Project
 
-Steps to run this project:
+Steps to run the backend:
 
-1. Run `npm i` command
-2. Setup database settings inside `ormconfig.json` file
-3. Run `npm start` command
+1. Navigate to the "Back-End" folder
+2. Run `npm install`
+3. Run `npm start` to start the backend server
+
--- Back-End/src/index.ts
+++ Back-End/src/index.ts
@@ -39,18 +39,6 @@ createConnection().then(async connection => {
     // start express server
     app.listen(4000);
 
-    // insert new users for test
-    await connection.manager.save(connection.manager.create(User, {
-        firstName: "Timber",
-        lastName: "Saw",
-        age: 27
-    }));
-    await connection.manager.save(connection.manager.create(User, {
-        firstName: "Phantom",
-        lastName: "Assassin",
-        age: 24
-    }));
-
     console.log("Express server has started on port 4000. Open http://localhost:4000/users to see results");
 
 }).catch(error => console.log(error));
