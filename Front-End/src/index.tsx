a3140ff19db93fea35ea922586f742b32b85bf3a
Arunraj Adlee <arunraj.adlee@hotmail.com>
2019-10-13 21:51:03 -0400

    Remove serviceworker.ts

--- Front-End/src/index.tsx
+++ Front-End/src/index.tsx
@@ -6,7 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { createBrowserHistory } from 'history';
 import App from './App';
 import Login from './components/Login';
-import * as serviceWorker from './serviceWorker';
 
 const history = createBrowserHistory();
 
@@ -23,8 +22,3 @@ ReactDOM.render(
     </Switch>
   </Router>, document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
