c3e93a8ab55e2099c027585c657d73c04525e365
Miguel Jimenez <migueleduardo298@hotmail.com>
2019-10-25 02:45:04 -0400

    New responsive search bar design

--- Front-End/src/layouts/Header/NavigationBar/navigationBar.tsx
+++ Front-End/src/layouts/Header/NavigationBar/navigationBar.tsx
@@ -1,81 +1,44 @@
 import React from 'react';
 import {
-  Navbar, Nav, Form, FormControl, Button, InputGroup,
+    Navbar, Nav, Form, FormControl, Button, InputGroup,
 } from 'react-bootstrap';
-import { faCartArrowDown, faSearch, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faCartArrowDown, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
 
-interface IStates {
-    searchOpen: boolean
-}
-
-class NavigationBar extends React.Component<{}, IStates> {
-    public readonly state: Readonly<IStates> = {
-      searchOpen: false,
-    }
-
-    public render() {
-      const { searchOpen } = this.state;
-      return (
+class NavigationBar extends React.Component<{}> {
+  public render() {
+    return (
         <div className="sticky-top">
-          <Navbar className="main-navbar" sticky="top" expand="sm">
-            <Navbar.Brand>
-              <Link
-                className="nav-link"
-                to={{
-                  pathname: '/',
-                }}
-              >
-                <h4>354TheStars</h4>
-              </Link>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ml-auto">
-                <Link
-                  className="nav-link"
-                  to={{
-                    pathname: '/listings',
-                  }}
-                >
-               VIEW LISTINGS
-                </Link>
-                <Link
-                  className="nav-link"
-                  to={{
-                    pathname: '/account',
-                  }}
-                >
-               MY ACCOUNT
-                </Link>
-                <Link
-                  className="nav-link"
-                  to={{
-                    pathname: '/cart',
-                  }}
-                >
-                  <FontAwesomeIcon icon={faCartArrowDown} />
-                </Link>
-                <Nav.Link><div onClick={() => this.setState({ searchOpen: !searchOpen })}><FontAwesomeIcon icon={faSearch} /></div></Nav.Link>
-              </Nav>
-              <Form className={searchOpen ? '' : 'd-none'} inline>
-                <InputGroup>
-                  <FormControl
-                    placeholder="Search..."
-                  />
-                  <InputGroup.Append>
-                    <Button variant="warning" className="styled-button">
-                      <FontAwesomeIcon icon={faArrowRight} />
-                    </Button>
-                  </InputGroup.Append>
-                </InputGroup>
-              </Form>
-            </Navbar.Collapse>
-          </Navbar>
+            <Navbar className="main-navbar" sticky="top" expand="sm">
+                <Navbar.Brand>
+                    <Link className="nav-link align-self-center mt-1" to={{ pathname: '/' }}>
+                        <h4>354TheStars</h4>
+                    </Link>
+                </Navbar.Brand>
+                <Form className="w-50 mr-3">
+                    <InputGroup>
+                        <FormControl type="text" placeholder="Search"/>
+                        <InputGroup.Append>
+                            <Button variant="outline-primary"><FontAwesomeIcon icon={faSearch} /></Button>
+                        </InputGroup.Append>
+                    </InputGroup>
+                </Form>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav>
+                        <Link className="nav-link align-self-center" to={{ pathname: '/account' }}>
+                            MY ACCOUNT
+                        </Link>
+                        <Link className="nav-link align-self-center mt-1" to={{ pathname: '/cart' }}>
+                            <FontAwesomeIcon icon={faCartArrowDown}/>
+                        </Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         </div>
-      );
-    }
+    );
+  }
 }
 
 export default NavigationBar;
