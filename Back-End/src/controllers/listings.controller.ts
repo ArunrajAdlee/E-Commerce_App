0c806c9a60c5451860342483fb3dd01825d2533b
Morteza Ahmadi <morinob93@gmail.com>
2019-10-30 18:42:29 -0400

    Address comments in the PR

--- Back-End/src/controllers/listings.controller.ts
+++ Back-End/src/controllers/listings.controller.ts
@@ -44,4 +44,18 @@ export class ListingsController {
 		const requestedCategory: number = parseInt(req.params.category);
 		return this.listingsRepository.find({ category: requestedCategory });
 	}
+
+	async allWithSearchQuery(req: Request, res: Response, next: NextFunction) {
+        var queries: Array<string> = req.params.searchQuery.split("+");
+        const allListings = await this.listingsRepository.find();
+        var requestedListings: Array<Listings>
+        for(var listing in allListings) {
+            for(var query in queries) {
+                if(allListings[listing].title.includes(query)) {
+                    requestedListings.push(allListings[listing]);
+                }
+            }
+        }
+        return requestedListings;
+    }
 }
