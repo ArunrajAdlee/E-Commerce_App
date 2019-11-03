a0a20d11f565812cf14c69541e80798e9b624ce7
Miguel Jimenez <migueleduardo298@hotmail.com>
2019-11-03 17:39:22 -0500

    Initial layout
    Create columns to display all items in query

--- Front-End/src/components/Listings/listings.tsx
+++ Front-End/src/components/Listings/listings.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Row, Col, Spinner } from 'react-bootstrap';
+import {
+  Row, Col, Spinner, Image,
+} from 'react-bootstrap';
 
 export interface Listing {
  id: number;
  name: string;
-
+ image: string;
+ price: number;
 }
 
 interface IProps {
@@ -15,23 +18,45 @@ interface IStates {
 
 }
 
-// Will finish when listings component is completed
+function listingThumbnail(listing: Listing) {
+  return (
+    <Col lg={4} md={6} className="pb-5">
+      <div>
+        <Image
+          src="http://maydayrooms.org/wp-content/uploads/kazimir-malevich-black-square.jpg"
+          height="120px"
+          width="120px"
+          alt={listing.name}
+          fluid
+        />
+        <div>
+          <h5>{listing.name}</h5>
+          <div>
+            <h6>
+              $0.00
+              {listing.price}
+            </h6>
+          </div>
+        </div>
+      </div>
+    </Col>
+  );
+}
 
 class Listings extends React.Component<IProps, IStates> {
   public readonly state: Readonly<IStates> = {
-  }
+  };
 
   public render() {
     const { listings } = this.props;
     return (
       <>
         {listings.length ? (
-          <Row>
-            <Col md={3} sm={12}>
-              {
-                 listings.map((listing) => (<p key={listing.id}>{listing.name}</p>))
-             }
-            </Col>
+          <Row className="pt-5">
+            {
+                 listings.map((listing) => (
+                   listingThumbnail(listing)))
+              }
           </Row>
         )
           : <Spinner animation="border" variant="warning" />}
