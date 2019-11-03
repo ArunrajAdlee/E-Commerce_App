2be79cdcaccd1bf9a0b2fb78e5c9dc2f6d5647af
Radhep <Radhep.Saba@gmail.com>
2019-11-03 15:28:17 -0500

    return user data on auth

--- Back-End/src/controllers/auth.controller.ts
+++ Back-End/src/controllers/auth.controller.ts
@@ -8,107 +8,128 @@ import { UserModel } from '../models/user.model';
 const { checkAuth } = require('../helpers/check-auth');
 
 export class AuthController {
-	private userRepository = getRepository(User);
-	private cookieName = 'access_token';
+  private userRepository = getRepository(User);
+  private cookieName = 'access_token';
 
-	async createUser(req: Request, res: Response, next: NextFunction) {
-		// Validate info in the future *****
-		const userExists = await this.userRepository.findOne({
-			username: req.body.username,
-		});
-		if (userExists) {
-			res.status(404).send('user already exists');
-			return;
-		}
+  async createUser(req: Request, res: Response, next: NextFunction) {
+    // Validate info in the future *****
+    const userExists = await this.userRepository.findOne({
+      username: req.body.username
+    });
+    if (userExists) {
+      res.status(404).send('user already exists');
+      return;
+    }
 
-		// Hash password
-		const salt = await bcrypt.genSalt(10);
-		const hashedPassword = await bcrypt.hash(req.body.password, salt);
-		const newUser: UserModel = {
-			username: req.body.username,
-			password: hashedPassword,
-		};
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const newUser: UserModel = {
+      username: req.body.username,
+      password: hashedPassword
+    };
 
-		// Save new user to the database
-		try {
-			const savedUser = await this.userRepository.save(newUser);
-			res.status(200).send({
-				message: 'sucessfully created the user',
-				savedUser,
-			});
-		} catch (error) {
-			res.status(404).send('could not create account');
-		}
-	}
+    // Save new user to the database
+    try {
+      const savedUser = await this.userRepository.save(newUser);
+      res.status(200).send({
+        message: 'sucessfully created the user',
+        savedUser
+      });
+    } catch (error) {
+      res.status(404).send('could not create account');
+    }
+  }
 
-	async login(req: Request, res: Response, next: NextFunction) {
-		// Check if user exists
-		const user = await this.userRepository.findOne({
-			username: req.body.username,
-		});
-		if (!user) {
-			res.status(404).send('user does not exist');
-			return;
-		}
+  async login(req: Request, res: Response, next: NextFunction) {
+    // Check if user exists
+    const user = await this.userRepository.findOne({
+      username: req.body.username
+    });
+    if (!user) {
+      res.status(404).send('user does not exist');
+      return;
+    }
 
-		// Validate password
-		const validPassword = await bcrypt.compare(
-			req.body.password,
-			user.password,
-		);
-		if (!validPassword) {
-			res.status(404).send('invalid password');
-			return;
-		}
+    // Validate password
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword) {
+      res.status(404).send('invalid password');
+      return;
+    }
 
-		// Create and return token
-		const token = jwt.sign(
-			{
-				username: user.username,
-				userId: user.id,
-			},
-			'secretKey',
-		);
-		// May need more information like expiration time (ask front-end peeps) ****
-		res.cookie(this.cookieName, token, {
-			maxAge: 3600000,
-			httpOnly: true,
-			// uncomment 'secure' when running in production
-			// secure: true
-		}).send('cookie set');
-	}
+    // Create and return token
+    const token = jwt.sign(
+      {
+        username: user.username,
+        id: user.id
+      },
+      'secretKey'
+    );
+    // May need more information like expiration time (ask front-end peeps) ****
+    res
+      .cookie(this.cookieName, token, {
+        maxAge: 3600000,
+        httpOnly: true
+        // uncomment 'secure' when running in production
+        // secure: true
+      })
+      .send('cookie set');
+  }
 
-	async remove(req: Request, res: Response, next: NextFunction) {
-		// Find user to delete
-		const userToRemove = await this.userRepository.findOne(req.params.id);
-		if (!userToRemove) {
-			res.status(404).send('error');
-			return;
-		}
+  async remove(req: Request, res: Response, next: NextFunction) {
+    // Find user to delete
+    const userToRemove = await this.userRepository.findOne(req.params.id);
+    if (!userToRemove) {
+      res.status(404).send('error');
+      return;
+    }
 
-		// Find user to delete
-		const removedUser = await this.userRepository.remove(userToRemove);
-		if (!removedUser) {
-			res.status(404).send('error');
-		} else {
-			res.status(200).send('successfully deleted');
-		}
-	}
+    // Find user to delete
+    const removedUser = await this.userRepository.remove(userToRemove);
+    if (!removedUser) {
+      res.status(404).send('error');
+    } else {
+      res.status(200).send('successfully deleted');
+    }
+  }
 
-	async getAuthStatus(req: Request, res: Response, next: NextFunction) {
-		let isAuthenticated = false;
-		const authenticatedUser: AuthModel = checkAuth(req);
-		if (authenticatedUser) {
-			isAuthenticated = true;
-		}
-		res.status(200).send({ isAuthenticated });
-	}
+  async getAuthStatus(req: Request, res: Response, next: NextFunction) {
+    let isAuthenticated = false;
+    let user: any;
+    //Check if the user is authenticated
+    const authenticatedUser: AuthModel = checkAuth(req);
 
-	async logout(req: Request, res: Response, next: NextFunction) {
-		const authenticatedUser: AuthModel = checkAuth(req);
-		if (authenticatedUser) {
-			res.clearCookie(this.cookieName);
-		}
-		res.status(200).end();
-	}
+    //If authenticated, retrieve user data
+    if (authenticatedUser) {
+      isAuthenticated = true;
+      const userDatabase = await this.userRepository.findOne({
+        id: authenticatedUser.id
+      });
+
+      //Set user data
+      if (userDatabase) {
+        user = {
+          username: userDatabase.username,
+          email: userDatabase.email,
+          first_name: userDatabase.first_name,
+          last_name: userDatabase.last_name,
+          brand_name: userDatabase.brand_name
+        };
+      }
+    }
+    //Return authentication status and user data
+    res.status(200).send({ isAuthenticated, user });
+  }
+
+  async logout(req: Request, res: Response, next: NextFunction) {
+    const authenticatedUser: AuthModel = checkAuth(req);
+    if (authenticatedUser) {
+      res.clearCookie(this.cookieName);
+    }
+    res.status(200).end();
+  }
 }
