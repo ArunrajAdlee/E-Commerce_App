bedb9fcb500a4c05fe2909d390212940e8807e11
Miguel Jimenez <migueleduardo298@hotmail.com>
2019-11-03 18:21:48 -0500

    Link to listing's page
    Link div to listing's page, and show error messages if image or price for a listing are unexistent.

--- Front-End/src/components/Listings/listings.tsx
+++ Front-End/src/components/Listings/listings.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   Row, Col, Spinner, Image,
 } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export interface Listing {
  id: number;
@@ -19,25 +20,33 @@ interface IStates {
 }
 
 function listingThumbnail(listing: Listing) {
+  let listingImage = 'https://3.bp.blogspot.com/-XB85UD145qE/V5buf22iv2I/AAAAAAAAA1I/8LBmpwNX-rU7ZjzrHOS2b0F_Pj0xqpHIQCLcB/s1600/nia.png';
+  if (listing.image) {
+    listingImage = listing.image;
+  }
+
+  let listingPrice = 'Price is unavailable';
+  if (listing.price) {
+    listingPrice = `${listing.price}`;
+  }
+
   return (
     <Col lg={4} md={6} className="pb-5">
       <div>
-        <Image
-          src="http://maydayrooms.org/wp-content/uploads/kazimir-malevich-black-square.jpg"
-          height="120px"
-          width="120px"
-          alt={listing.name}
-          fluid
-        />
-        <div>
-          <h5>{listing.name}</h5>
+        {/* Link to listing view */}
+        <Link to={`/listing/${listing.name}`} className="text-decoration-none text-dark">
+          <Image
+            src={listingImage}
+            alt={listing.name}
+            fluid
+          />
           <div>
-            <h6>
-              $0.00
-              {listing.price}
-            </h6>
+            <h5 className="pt-2">{listing.name}</h5>
+            <div>
+              <h6>{listingPrice}</h6>
+            </div>
           </div>
-        </div>
+        </Link>
       </div>
     </Col>
   );
