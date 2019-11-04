0da58d59fa208479335bcd6b0e5204033b867735
Miguel Jimenez <migueleduardo298@hotmail.com>
2019-11-03 23:47:14 -0500

    Add pagination

--- Front-End/src/components/Listings/listings.tsx
+++ Front-End/src/components/Listings/listings.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Row, Col, Spinner, Image,
+  Row, Col, Spinner, Image, Pagination,
 } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
@@ -16,10 +16,13 @@ interface IProps {
 }
 
 interface IStates {
-
+  currentPage: number,
+  listingsPerPage: number,
 }
 
+// Returns the thumbnail of each listing in index
 function listingThumbnail(listing: Listing) {
+  // Placeholder until there is a database with an error image to point to
   let listingImage = 'https://3.bp.blogspot.com/-XB85UD145qE/V5buf22iv2I/AAAAAAAAA1I/8LBmpwNX-rU7ZjzrHOS2b0F_Pj0xqpHIQCLcB/s1600/nia.png';
   if (listing.image) {
     listingImage = listing.image;
@@ -53,22 +56,51 @@ function listingThumbnail(listing: Listing) {
 }
 
 class Listings extends React.Component<IProps, IStates> {
-  public readonly state: Readonly<IStates> = {
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = {
+      currentPage: 1,
+      listingsPerPage: 12,
+    };
+  }
+
+  handlePagination = (event: any) => {
+    this.setState({
+      currentPage: Number(event.target.text),
+    });
   };
 
   public render() {
     const { listings } = this.props;
+    const { currentPage, listingsPerPage } = this.state;
+
+    // Logic for displaying current listings
+    const indexOfLastListing = currentPage * listingsPerPage;
+    const indexOfFirstListing = indexOfLastListing - listingsPerPage;
+    const currentListings = listings.slice(indexOfFirstListing, indexOfLastListing);
+
+    // Initialize page numbers
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(listings.length / listingsPerPage); i++) {
+      pageNumbers.push(
+        <Pagination.Item key={i} active={i === currentPage} onClick={this.handlePagination}>
+          {i}
+        </Pagination.Item>,
+      );
+    }
+
     return (
       <>
         {listings.length ? (
           <Row className="pt-5">
-            {
-                 listings.map((listing) => (
-                   listingThumbnail(listing)))
-              }
+            { currentListings.map((listing) => (listingThumbnail(listing))) }
           </Row>
         )
           : <Spinner animation="border" variant="warning" />}
+        <div>
+          <Pagination className="float-right">{pageNumbers}</Pagination>
+        </div>
       </>
     );
   }
