1999f8aee4a171955cb8af99a72029b3a09716ab
Miguel Jimenez <migueleduardo298@hotmail.com>
2019-11-05 02:34:09 -0500

    Add a key to each column containing a listing
    Added a key to each column containing a listing, and made functions listingStatus and listingThumbnail part of the Listings class

--- Front-End/src/components/Listings/listings.tsx
+++ Front-End/src/components/Listings/listings.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Row, Col, Spinner, Image, Pagination } from 'react-bootstrap';
+import {
+  Row, Col, Spinner, Image, Pagination,
+} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export interface Listing {
@@ -20,63 +22,62 @@ interface IStates {
   listingsPerPage: number;
 }
 
-// Displays if listing is available or unavailable
-function listingStatus(listing: Listing) {
-  let listingPrice: any = listing.price;
-  let textColor = 'text-dark';
-
-  if (!listing.isAvailable) {
-    listingPrice = 'Unavailable';
-    textColor = 'text-danger';
-  }
-
-  return <h6 className={textColor}>{listingPrice}</h6>;
-}
-
-// Returns the thumbnail of each listing in index
-function listingThumbnail(listing: Listing) {
-  // Placeholder until there is a database with an error image to point to
-  let listingImage =
-    'https://3.bp.blogspot.com/-XB85UD145qE/V5buf22iv2I/AAAAAAAAA1I/8LBmpwNX-rU7ZjzrHOS2b0F_Pj0xqpHIQCLcB/s1600/nia.png';
-  if (listing.thumbnail) {
-    listingImage = listing.thumbnail;
-  }
-
-  return (
-    <Col lg={4} md={6} className="pb-5">
-      <div>
-        {/* Link to listing view */}
-        <Link
-          to={`/listing/${listing.id}`}
-          className="text-decoration-none text-dark"
-        >
-          <Image src={listingImage} alt={listing.name} fluid />
-          <div>
-            <h5 className="pt-2">{listing.name}</h5>
-            <div>{listingStatus(listing)}</div>
-          </div>
-        </Link>
-      </div>
-    </Col>
-  );
-}
-
 class Listings extends React.Component<IProps, IStates> {
   constructor(props: IProps) {
     super(props);
 
     this.state = {
       currentPage: 1,
-      listingsPerPage: 12
+      listingsPerPage: 12,
     };
   }
 
   handlePagination = (event: any) => {
     if (event.target.text !== undefined) {
       this.setState({
-        currentPage: Number(event.target.text)
+        currentPage: Number(event.target.text),
       });
     }
+  };
+
+  // Displays if listing is available or unavailable
+  listingStatus = (listing: Listing) => {
+    let listingPrice: any = listing.price;
+    let textColor = 'text-dark';
+
+    if (!listing.isAvailable) {
+      listingPrice = 'Unavailable';
+      textColor = 'text-danger';
+    }
+
+    return <h6 className={textColor}>{listingPrice}</h6>;
+  };
+
+  // Returns the thumbnail of each listing in index
+  listingThumbnail = (listing: Listing) => {
+    // Placeholder until there is a database with an error image to point to
+    let listingImage = 'https://3.bp.blogspot.com/-XB85UD145qE/V5buf22iv2I/AAAAAAAAA1I/8LBmpwNX-rU7ZjzrHOS2b0F_Pj0xqpHIQCLcB/s1600/nia.png';
+    if (listing.thumbnail) {
+      listingImage = listing.thumbnail;
+    }
+
+    return (
+      <Col key={listing.id} lg={4} md={6} className="pb-5">
+        <div>
+          {/* Link to listing view */}
+          <Link
+            to={`/listing/${listing.id}`}
+            className="text-decoration-none text-dark"
+          >
+            <Image src={listingImage} alt={listing.name} fluid />
+            <div>
+              <h5 className="pt-2">{listing.name}</h5>
+              <div>{this.listingStatus(listing)}</div>
+            </div>
+          </Link>
+        </div>
+      </Col>
+    );
   };
 
   public render() {
@@ -88,7 +89,7 @@ class Listings extends React.Component<IProps, IStates> {
     const indexOfFirstListing = indexOfLastListing - listingsPerPage;
     const currentListings = listings.slice(
       indexOfFirstListing,
-      indexOfLastListing
+      indexOfLastListing,
     );
 
     // Initialize page numbers
@@ -101,7 +102,7 @@ class Listings extends React.Component<IProps, IStates> {
           onClick={this.handlePagination}
         >
           {i}
-        </Pagination.Item>
+        </Pagination.Item>,
       );
     }
 
@@ -109,7 +110,7 @@ class Listings extends React.Component<IProps, IStates> {
       <>
         {listings.length ? (
           <Row className="pt-5">
-            {currentListings.map(listing => listingThumbnail(listing))}
+            {currentListings.map((listing) => this.listingThumbnail(listing))}
           </Row>
         ) : (
           <Spinner animation="border" variant="warning" />
