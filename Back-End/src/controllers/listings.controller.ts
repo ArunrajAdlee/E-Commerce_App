c153a4bcec6d7bc60afb2d60b637810024dc77c1
Radhep <Radhep.Saba@gmail.com>
2019-11-06 00:57:26 -0500

    revert image changes

--- Back-End/src/controllers/listings.controller.ts
+++ Back-End/src/controllers/listings.controller.ts
@@ -39,7 +39,7 @@ export class ListingsController {
 
       const cloudImage = await cloudinary.uploader.upload(
         reqImage.tempFilePath,
-        { unique_filename: true, width: 540, height: 580, crop: 'fit' }
+        { unique_filename: true, width: 540, height: 580 }
       );
       const imageURL = cloudImage.url;
       //Crop the size of the image for the thumbnail (225 x 225)
@@ -49,7 +49,7 @@ export class ListingsController {
       );
       const thumbnailURL =
         imageURL.substr(0, sizeInputLocation) +
-        '/w_255,h_270,c_fit' +
+        '/w_255,h_270' +
         imageURL.substr(sizeInputLocation, imageURL.length - 1);
 
       const newProduct: ListingsModel = {
