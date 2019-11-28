b9e8c66e9b51de3d0939c41a26ce299509aeb744
RadhepS <34665674+RadhepS@users.noreply.github.com>
2019-11-27 23:37:12 -0500

    Update TableScripts.sql

--- Database/TableScripts.sql
+++ Database/TableScripts.sql
@@ -83,14 +83,14 @@ CREATE TABLE `cart` (
 CREATE TABLE `reviews` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `user_id` int NOT NULL,
-  `listing_id` int NOT NULL,
+  `seller_id` int NOT NULL,
   `title` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
   `rating` int
 );
 
 ALTER TABLE `reviews` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
-ALTER TABLE `reviews` ADD FOREIGN KEY (`listing_id`) REFERENCES `listings` (`id`);
+ALTER TABLE `reviews` ADD FOREIGN KEY (`seller_id`) REFERENCES `user` (`id`);
 
 ALTER TABLE `user` ADD FOREIGN KEY (`address_id`) REFERENCES `address` (`id`);
 
