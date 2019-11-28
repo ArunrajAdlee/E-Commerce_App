d856482b31b1d41a41c4463df9988680e2a8d53b
Radhep <Radhep.Saba@gmail.com>
2019-11-27 20:19:27 -0500

    scripts

---
+++ Database/ListingsDataScript.sql
@@ -0,0 +1,36 @@
+INSERT INTO categories (name) VALUES ('Shoes');
+INSERT INTO categories (name) VALUES ('Shirts');
+INSERT INTO categories (name) VALUES ('Phones');
+INSERT INTO categories (name) VALUES ('Other');
+
+INSERT INTO listings (title, stock_count, image, thumbnail, description, price, quantity_sold, status, user_id, category, username, category_name) 
+VALUES ('Google Home Mini', 185, 'http://res.cloudinary.com/ddubs1/image/upload/v1573189530/wstlbrpmop9hzighq9ln.jpg', 'http://res.cloudinary.com/ddubs1/image/upload/w_255,h_270/v1573189530/wstlbrpmop9hzighq9ln.jpg',
+'Brand new', 50, 0, 1, 1, 4, 'admin', 'Other'),
+('JanSport Backpack',65,'http://res.cloudinary.com/ddubs1/image/upload/v1573189458/z1apwqdxigikcd9ynolv.jpg','http://res.cloudinary.com/ddubs1/image/upload/w_255,h_270/v1573189458/z1apwqdxigikcd9ynolv.jpg',
+'Brand New',30,0,1,1,4,'admin','Other'),
+('Samsung 40 Inch LED TV',550,'http://res.cloudinary.com/ddubs1/image/upload/v1573189362/n94bea5nhaqmbkyl91zw.jpg','http://res.cloudinary.com/ddubs1/image/upload/w_255,h_270/v1573189362/n94bea5nhaqmbkyl91zw.jpg',
+'Brand New',680,0,1,1,4,'admin','Other'),
+('Jurassic Express',120,'http://res.cloudinary.com/ddubs1/image/upload/v1573189112/bxu1elasvz1ceqd2hh66.png','http://res.cloudinary.com/ddubs1/image/upload/w_255,h_270/v1573189112/bxu1elasvz1ceqd2hh66.png',
+'Brand New',20,0,1,1,2,'admin','Shirts'),
+('Supreme Box Logo',12,'http://res.cloudinary.com/ddubs1/image/upload/v1573189075/ktk4cm742hbli6krhug7.jpg','http://res.cloudinary.com/ddubs1/image/upload/w_255,h_270/v1573189075/ktk4cm742hbli6krhug7.jpg',
+'Brand New',275,0,1,1,2,'admin','Shirts'),
+('OnePlus 7',1500,'http://res.cloudinary.com/ddubs1/image/upload/v1573188573/yq3w1xcyqgidpafypnu8.jpg','http://res.cloudinary.com/ddubs1/image/upload/w_255,h_270/v1573188573/yq3w1xcyqgidpafypnu8.jpg',
+'Brand New',700,0,1,1,3,'admin','Phones'),
+('Samsung S10 Plus',800,'http://res.cloudinary.com/ddubs1/image/upload/v1573188540/zpvievkf1pb83vz2ek5o.jpg','http://res.cloudinary.com/ddubs1/image/upload/w_255,h_270/v1573188540/zpvievkf1pb83vz2ek5o.jpg',
+'Brand New',1000,0,1,1,3,'admin','Phones'),
+('iPhone 11 Pro Max',900,'http://res.cloudinary.com/ddubs1/image/upload/v1573188467/i6hr6hetysmhbna6bqro.jpg','http://res.cloudinary.com/ddubs1/image/upload/w_255,h_270/v1573188467/i6hr6hetysmhbna6bqro.jpg',
+'Brand New',1200,0,1,1,3,'admin','Phones'),
+('Yeezy Boost 350 V2 - Oreo',50,'http://res.cloudinary.com/ddubs1/image/upload/v1573186235/xsyjap7gv5prnwqfz2nu.jpg','http://res.cloudinary.com/ddubs1/image/upload/w_255,h_270/v1573186235/xsyjap7gv5prnwqfz2nu.jpg',
+'Brand New',325,0,1,1,1,'admin','Shoes'),
+('Old Skool Vans - Navy',750,'http://res.cloudinary.com/ddubs1/image/upload/v1573186120/ejuaixp3zkks79xalq5r.jpg','http://res.cloudinary.com/ddubs1/image/upload/w_255,h_270/v1573186120/ejuaixp3zkks79xalq5r.jpg',
+'Brand New',60,0,1,1,1,'admin','Shoes'),
+('Stan Smith - Orange White',350,'http://res.cloudinary.com/ddubs1/image/upload/v1573185895/z0txtovsckjqrkxxay95.jpg','http://res.cloudinary.com/ddubs1/image/upload/w_255,h_270/v1573185895/z0txtovsckjqrkxxay95.jpg',
+'Brand New',80,0,1,1,1,'admin','Shoes'),
+('NMD_R1 - Black',200,'http://res.cloudinary.com/ddubs1/image/upload/v1573185637/pl1epmghwfbiecfeprc0.jpg','http://res.cloudinary.com/ddubs1/image/upload/w_255,h_270/v1573185637/pl1epmghwfbiecfeprc0.jpg',
+'Brand New',125,0,1,1,1,'admin','Shoes'),
+('Converse Chuck Taylor Low - Denim Blue',600,'http://res.cloudinary.com/ddubs1/image/upload/v1573185593/n8gshfnwfkaojfntmxr3.png','http://res.cloudinary.com/ddubs1/image/upload/w_255,h_270/v1573185593/n8gshfnwfkaojfntmxr3.png',
+'Brand New',50,0,1,1,1,'admin','Shoes'),
+('Air Jordan 1 Retro - Orange',15,'http://res.cloudinary.com/ddubs1/image/upload/v1573185381/gith9igmymb2y8wqrrmx.png','http://res.cloudinary.com/ddubs1/image/upload/w_255,h_270/v1573185381/gith9igmymb2y8wqrrmx.png',
+'Brand New',150,0,1,1,1,'admin','Shoes');
+
+
---
+++ Database/TableScripts.sql
@@ -0,0 +1,112 @@
+
+
+#SET FOREIGN_KEY_CHECKS=1;
+
+
+CREATE TABLE `user` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `username` VARCHAR(255) UNIQUE,
+  `password` VARCHAR(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `brand_name` varchar(255),
+  `date_of_birth` date,
+  `phone_number` VARCHAR(255),
+  `age` integer,
+  `address_id` int NOT NULL
+);
+
+CREATE TABLE `address` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `street_name` varchar(255) NOT NULL,
+  `street_number` integer NOT NULL,
+  `unit_number` varchar(255),
+  `city` varchar(255) NOT NULL,
+  `province` varchar(255) NOT NULL,
+  `postal_code` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL
+);
+
+CREATE TABLE `listings` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `thumbnail` VARCHAR(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `price` double NOT NULL,
+  `stock_count` int NOT NULL,
+  `quantity_sold` int NOT NULL DEFAULT 0,
+  `status` BOOLEAN NOT NULL,
+  `user_id` int NOT NULL,
+  `username` VARCHAR(255) NOT NULL,
+  `category` int NOT NULL,
+  `category_name` VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE `categories` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE `order` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+   `buyer_id` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `shipping_type` varchar(255) NOT NULL,
+  `shipped_to` int NOT NULL,
+  `shipping_status` varchar(255) NOT NULL,
+  `total_price_before_tax` double NOT NULL,
+  `total_tax` double NOT NULL,
+  `total_price` double NOT NULL,
+  `total_fee` double NOT NULL
+  );
+  
+  CREATE TABLE `order_details` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `order_id` int(11) NOT NULL,
+  `listing_id` int(11) NOT NULL,
+  `seller_id` int(11) NOT NULL,
+  `purchase_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `quantity` int(11) NOT NULL,
+  `price_before_tax` double DEFAULT NULL,
+  `tax` double DEFAULT NULL,
+  `price_after_tax` double DEFAULT NULL,
+  `listing_fee` double DEFAULT NULL
+); 
+
+
+  CREATE TABLE `cart` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `listing_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `quantity` int(11) DEFAULT NULL
+);
+
+CREATE TABLE `reviews` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `listing_id` int NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `rating` int
+);
+
+ALTER TABLE `reviews` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+ALTER TABLE `reviews` ADD FOREIGN KEY (`listing_id`) REFERENCES `listings` (`id`);
+
+ALTER TABLE `user` ADD FOREIGN KEY (`address_id`) REFERENCES `address` (`id`);
+
+ALTER TABLE `cart` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+ALTER TABLE `cart` ADD FOREIGN KEY (`listing_id`) REFERENCES `listings` (`id`);
+
+ALTER TABLE `order_details` ADD FOREIGN KEY (`order_id`) REFERENCES `order` (`id`);
+
+ALTER TABLE `order_details` ADD FOREIGN KEY (`seller_id`) REFERENCES `user` (`id`);
+
+ALTER TABLE `listings` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+ALTER TABLE `order` ADD FOREIGN KEY (`shipped_to`) REFERENCES `address` (`id`);
+ALTER TABLE `order` ADD FOREIGN KEY (`buyer_id`) REFERENCES `user` (`id`);
+
+
