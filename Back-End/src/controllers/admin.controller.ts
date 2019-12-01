c8335bd4cbe5401603614d22cbad2fad778e0086
Radhep <Radhep.Saba@gmail.com>
2019-11-30 23:22:38 -0500

    admin stuff

--- Back-End/src/controllers/admin.controller.ts
+++ Back-End/src/controllers/admin.controller.ts
@@ -4,6 +4,7 @@ import { getRepository } from 'typeorm';
 import { AuthModel } from '../models/auth.model';
 import { Ads } from '../entity/ads.entity';
 import { OrderDetails } from '../entity/orderDetails.entity';
+import { User } from '../entity/user.entity';
 // tslint:disable-next-line
 const { checkAuth } = require('../helpers/check-auth');
 
@@ -36,13 +37,29 @@ export class AdminController {
       return;
     }
 
-    // const highestOrders = await getRepository(OrderDetails).createQueryBuilder('order_details')
-    // .select('SUM(order_details.price_before_tax)', 'totalSumPerUser')
-    // .groupBy('order_details.seller_id')
-    // .orderBy('totalSumPerUser', 'DESC')
-    // .getRawMany();
+    const totalAmounts = await getRepository(OrderDetails).createQueryBuilder('order_details')
+    .select(['SUM(price_before_tax) AS sum_price_before_tax', 
+            'SUM(price_after_tax) AS sum_price_after_tax',
+            'SUM(listing_fee) AS sum_listing_fee']
+    ).getRawMany();
 
-    // console.log(highestOrders);
+    console.log(totalAmounts);
+
+    const highestOrders = await getRepository(OrderDetails).createQueryBuilder('order_details')
+    .innerJoin(User, 'user', 'user.id = order_details.seller_id')
+    .select(['order_details.seller_id', 'user.username', 'SUM(order_details.price_before_tax) AS totalSum'])
+    .groupBy('order_details.seller_id')
+    .orderBy('totalSum', 'DESC')
+    .limit(10)
+    .getRawMany();
+    
+   
+
+// const user = await getRepository(User).createQueryBuilder("user")
+//     .innerJoin(OrderDetails,  "orderDetails", "orderDetails.seller_id = user.id")
+//     .select(['orderDetails.price_after_tax'])
+//     .getRawMany();
+//     console.log(user);
 
   }
 }
