ccec48404d35d4124df0a2704c3980c0f6bc74ab
Timothy <exzos_graalonline@hotmail.com>
2019-12-01 17:13:22 -0500

    checkout address fix attempt

--- Back-End/src/controllers/address.controller.ts
+++ Back-End/src/controllers/address.controller.ts
@@ -16,15 +16,48 @@ export class AddressController {
                province: req.body.province,
                postal_code: req.body.postalCode,
                country: req.body.country
-        }
+        };
 
-        const address = await this.addressRepository.save(newAddress);
-        if(!address) {
-            res.status(404).send("error creating address");
+        //Check if identical address is already in db
+        const existingAddress = await this.addressRepository
+          .createQueryBuilder("Address")
+          .select( //Makes sure fields returned have same names
+            "street_name AS street_name, " +
+            "street_number AS street_number, " +
+            "unit_number AS unit_number, " +
+            "city AS city, " +
+            "province AS province, " +
+            "postal_code AS postal_code, " +
+            "country AS country, " +
+            "id as id")
+          .where(
+            "LOWER(street_name) = LOWER(:streetAddress) AND " + 
+            "street_number = :streetNumber AND " +
+            "unit_number = :unitNumber AND " +
+            "LOWER(city) = LOWER(:city) AND " + 
+            "LOWER(province) = LOWER(:province) AND " +
+            "REPLACE(LOWER(postal_code), ' ', '') = REPLACE(LOWER(:postalCode), ' ', '') AND " +
+            "LOWER(country) = LOWER(:country)", 
+            { streetAddress: newAddress.street_name, 
+              streetNumber: newAddress.street_number, 
+              unitNumber: newAddress.unit_number,
+              city: newAddress.city,
+              province: newAddress.province,
+              postalCode: newAddress.postal_code,
+              country: newAddress.country})
+          .getRawOne();
+
+        let address;
+        if(!existingAddress) {
+            address = await this.addressRepository.save(newAddress);
+        }
+        else {
+            address = existingAddress;
         }
 
         res.status(200).send({
-            address: address
+            address: address,
+            created: !existingAddress
         });
     } 
 }
